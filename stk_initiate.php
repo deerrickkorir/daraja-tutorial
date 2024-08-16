37d2753b17a01cef3243caf727a588cab05a87cb
Derrick Korir <derrickroz4227@gmail.com>
2024-08-16 12:03:34 +0300

    finish commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -1,48 +1,45 @@
 <?php
 if(isset($_POST['submit'])){
 
-
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'dTfQANAtqn6zSF9ipcQWTLxlL7oRWyJKxXyiuXG4a5YkZSXq'; //Fill with your app Consumer Key
+  $consumerSecret = 'ZGUFh4XjJbXNsoapTVXWgOwiMwe9fGepo2LzaaNDb46iWskbPr7riwyfGQHhY2SM'; // Fill with your app Secret
 
-  # define the variales
-  # provide the following details, this part is found on your test credentials on the developer account
+  # define the variables
   $BusinessShortCode = '174379';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
   
   /*
     This are your info, for
-    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
-    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
-    TransactionDesc can be anything, probably a better description of or the transaction
-    $Amount this is the total invoiced amount, Any amount here will be 
-    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
-    for developer/test accounts, this money will be reversed automatically by midnight.
+    $PartyA should be the ACTUAL client's phone number or your phone number, format 2547********
+    $AccountReference, it may be invoice number, account number etc on production systems, but for test just put anything
+    TransactionDesc can be anything, probably a better description of the transaction
+    $Amount this is the total invoiced amount, Any amount here will be actually deducted from a client's side/your test phone number once the PIN has been entered to authorize the transaction. 
+    For developer/test accounts, this money will be reversed automatically by midnight.
   */
   
    $PartyA = $_POST['phone']; // This is your phone number, 
-  $AccountReference = '2255';
-  $TransactionDesc = 'Test Payment';
-  $Amount = $_POST['amount'];
+   $AccountReference = 'EPIC EVENTS';
+   $TransactionDesc = 'Test Payment';
+   $Amount = $_POST['amount'];;
  
   # Get the timestamp, format YYYYmmddhms -> 20181004151020
   $Timestamp = date('YmdHis');    
   
-  # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+  # Get the base64 encoded string -> $Password. The passkey is the M-PESA Public Key
   $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
 
   # header for access token
   $headers = ['Content-Type:application/json; charset=utf8'];
 
-    # M-PESA endpoint urls
+    # M-PESA endpoint URLs
   $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
-  # callback url
-  $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+  # callback URL
+  $CallBackURL = 'https://4d83-41-212-123-118.ngrok-free.app/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -55,7 +52,7 @@ if(isset($_POST['submit'])){
   $access_token = $result->access_token;  
   curl_close($curl);
 
-  # header for stk push
+  # header for STK push
   $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
 
   # initiating the transaction
@@ -64,7 +61,7 @@ if(isset($_POST['submit'])){
   curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
 
   $curl_post_data = array(
-    //Fill in the request parameters with valid values
+    // Fill in the request parameters with valid values
     'BusinessShortCode' => $BusinessShortCode,
     'Password' => $Password,
     'Timestamp' => $Timestamp,
